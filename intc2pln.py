33d4b8aa5e8d5d168c82dfcbf83ef5fc745716cb
Michał <mduzin@o2.pl>
2018-03-13 21:34:27 +0100

    PEP8 naming convention

--- intc2pln.py
+++ intc2pln.py
@@ -24,173 +24,174 @@ informational purposes, not for trading purposes or advice, and is delayed.
 To see all exchange delays and terms of use, please see our disclaimer.
 """
 
-Url = "https://marketdata.websol.barchart.com/getHistory.json"
-ApiKey = "de845f7902386646116b91433e4c2c2e"
+URL = "https://marketdata.websol.barchart.com/getHistory.json"
+API_KEY = "de845f7902386646116b91433e4c2c2e"
 
-Periods = ['1d', '5d', '1M', '3M', '6M', '1Y', '2Y']
+PERIODS = ['1d', '5d', '1M', '3M', '6M', '1Y', '2Y']
 
-CommonParameters = {'order': 'asc'}
+COMMON_PARAMETERS = {'order': 'asc'}
 
-PeriodParameters = {'1d': {'type': 'minutes', 'interval': '15'},
-                    '5d': {'type': 'minutes', 'interval': '30'},
-                    '1M': {'type': 'minutes', 'interval': '30'},
-                    '3M': {'type': 'daily'},
-                    '6M': {'type': 'daily'},
-                    '1Y': {'type': 'daily'},
-                    '2Y': {'type': 'daily'}}
+PERIOD_PARAMETERS = {'1d': {'type': 'minutes', 'interval': '15'},
+                     '5d': {'type': 'minutes', 'interval': '30'},
+                     '1M': {'type': 'minutes', 'interval': '30'},
+                     '3M': {'type': 'daily'},
+                     '6M': {'type': 'daily'},
+                     '1Y': {'type': 'daily'},
+                     '2Y': {'type': 'daily'}}
 
-TimeDiffs = {'1d': datetime.timedelta(days=1),
-             '5d': datetime.timedelta(days=5),
-             '1M': datetime.timedelta(days=31),
-             '3M': datetime.timedelta(days=3*31),
-             '6M': datetime.timedelta(days=6*31),
-             '1Y': datetime.timedelta(days=365),
-             '2Y': datetime.timedelta(days=2*365)}
+TIME_DIFFS = {'1d': datetime.timedelta(days=1),
+              '5d': datetime.timedelta(days=5),
+              '1M': datetime.timedelta(days=31),
+              '3M': datetime.timedelta(days=3*31),
+              '6M': datetime.timedelta(days=6*31),
+              '1Y': datetime.timedelta(days=365),
+              '2Y': datetime.timedelta(days=2*365)}
 
 # we need to expand timespan to make sure we fetch all necessary data while
 # dealing with timezones
-MarketTimeDiffs = {'1d': datetime.timedelta(days=1 + 2),
-                   '5d': datetime.timedelta(days=5 + 4),
-                   '1M': datetime.timedelta(days=31 + 5),
-                   '3M': datetime.timedelta(days=3*31 + 5),
-                   '6M': datetime.timedelta(days=6*31 + 5),
-                   '1Y': datetime.timedelta(days=365 + 7),
-                   '2Y': datetime.timedelta(days=2*365 + 7)}
+MARKET_TIME_DIFFS = {'1d': datetime.timedelta(days=1 + 2),
+                     '5d': datetime.timedelta(days=5 + 4),
+                     '1M': datetime.timedelta(days=31 + 5),
+                     '3M': datetime.timedelta(days=3*31 + 5),
+                     '6M': datetime.timedelta(days=6*31 + 5),
+                     '1Y': datetime.timedelta(days=365 + 7),
+                     '2Y': datetime.timedelta(days=2*365 + 7)}
 
 # Plot resolution
-DeltaDatetimes = {'1d': datetime.timedelta(minutes=15),
-                  '5d': datetime.timedelta(minutes=30),
-                  '1M': datetime.timedelta(minutes=30),
-                  '3M': datetime.timedelta(days=1),
-                  '6M': datetime.timedelta(days=1),
-                  '1Y': datetime.timedelta(days=1),
-                  '2Y': datetime.timedelta(days=1)}
+DELTA_DATETIMES = {'1d': datetime.timedelta(minutes=15),
+                   '5d': datetime.timedelta(minutes=30),
+                   '1M': datetime.timedelta(minutes=30),
+                   '3M': datetime.timedelta(days=1),
+                   '6M': datetime.timedelta(days=1),
+                   '1Y': datetime.timedelta(days=1),
+                   '2Y': datetime.timedelta(days=1)}
 
 # TODO: Add plot params for each period
-PlotParameters = {}
+PLOT_PARAMETERS = {}
 
-Symbols = {'Intel': 'INTC',
+SYMBOLS = {'Intel': 'INTC',
            'Currency': '^USDPLN'}
 
-Response_Status_Codes = {'OK': 200,
+RESPONSE_STATUS_CODES = {'OK': 200,
                          'Bad Request ': 400,
                          'Internal Server Error': 500}
 
 
 # Replace from last occurance in string
-def rreplace(S, Old, New, Occurrence):
-    Li = S.rsplit(Old, Occurrence)
+def rreplace(Str, Old, New, Occurrence):
+    Li = Str.rsplit(Old, Occurrence)
     return New.join(Li)
 
 
 # Function returns start date in fixed string format for our stock market query
-def GetStartDatetime(Period):
-    StartDate = datetime.datetime.today() - MarketTimeDiffs[Period]
+def get_start_datetime(period):
+    start_date = datetime.datetime.today() - MARKET_TIME_DIFFS[period]
     # +0100 add Polish timezone information
-    StartDateStringTemplate = ("{Year:04d}"
-                               "{Month:02d}"
-                               "{Day:02d}"
-                               "{Hour:02d}"
-                               "{Minute:02d}"
-                               "{Second:02d}"
+    start_date_str_template = ("{year:04d}"
+                               "{month:02d}"
+                               "{day:02d}"
+                               "{hour:02d}"
+                               "{minute:02d}"
+                               "{second:02d}"
                                "+01:00")
 
-    # StartDateStringTemplate = ("{Year:04d}"
-    #                           "{Month:02d}"
-    #                           "{Day:02d}"
-    #                           "{Hour:02d}"
-    #                           "{Minute:02d}"
-    #                           "{Second:02d}")
+    # start_date_str_template = ("{Year:04d}"
+    #                               "{Month:02d}"
+    #                               "{Day:02d}"
+    #                               "{Hour:02d}"
+    #                               "{Minute:02d}"
+    #                               "{Second:02d}")
 
-    StartDateString = StartDateStringTemplate.format(Year=StartDate.year,
-                                                     Month=StartDate.month,
-                                                     Day=StartDate.day,
-                                                     Hour=StartDate.hour,
-                                                     Minute=StartDate.minute,
-                                                     Second=StartDate.second)
+    start_date_str = start_date_str_template.format(year=start_date.year,
+                                                    month=start_date.month,
+                                                    day=start_date.day,
+                                                    hour=start_date.hour,
+                                                    minute=start_date.minute,
+                                                    second=start_date.second)
 
-    return StartDateString
+    return start_date_str
 
 
 # Function gathers parameters for stock market query
-def PrepareRequestParameters(Period, Symbol):
-    Request = {'apikey': ApiKey,
-               'symbol': Symbol,
-               'startDate': GetStartDatetime(Period)}
-    Request.update(PeriodParameters[Period])
-    Request.update(CommonParameters)
-    return Request
+def prepare_request_parameters(period, symbol):
+    request = {'apikey': API_KEY,
+               'symbol': symbol,
+               'startDate': get_start_datetime(period)}
+    request.update(PERIOD_PARAMETERS[period])
+    request.update(COMMON_PARAMETERS)
+    return request
 
 
 # Function sends stock market query and get response
-def GetWebData(Period, Symbol):
-    Parameters = PrepareRequestParameters(Period, Symbol)
-    print(Parameters)
-    Response = requests.get(Url, params=Parameters)
-    return Response
+def get_web_data(period, symbol):
+    parameters = prepare_request_parameters(period, symbol)
+    print(parameters)
+    response = requests.get(URL, params=parameters)
+    return response
 
 
 # Wrapper for sending INTC query
-def GetIntelData(Period):
-    return GetWebData(Period, Symbols['Intel'])
+def get_intel_data(period):
+    return get_web_data(period, SYMBOLS['Intel'])
 
 
 # Wrapper for sending ^USDPLN query
-def GetCurrencyData(Period):
-    return GetWebData(Period, Symbols['Currency'])
+def get_currency_data(period):
+    return get_web_data(period, SYMBOLS['Currency'])
 
 
 # Function reshape data format into data[key][...]
-def ExtractData(Keys, Data):
-    return [[record[key] for record in Data] for key in Keys]
+def extract_data(keys, data):
+    return [[record[key] for record in data] for key in keys]
 
 
 # we need to convert timestamp from Web API's barchart format e.g:
 # '2018-02-13T00:00:00-06:00' to datetime.datetime acceptable format:
 # '2018-02-13T00:00:00:-0600'
-def ConvertTimestamp(Timestamp):
+def convert_timestamp(timestamp):
     # TODO: do it more pythonic
-    if Timestamp[-6] == '+':
-        ConvertedTimestamp = rreplace(Timestamp, "+", ":+", 1)
+    if timestamp[-6] == '+':
+        converted_timestamp = rreplace(timestamp, "+", ":+", 1)
     else:
-        ConvertedTimestamp = rreplace(Timestamp, "-", ":-", 1)
-    ConvertedTimestamp = rreplace(ConvertedTimestamp, ":", "", 1)
-    return ConvertedTimestamp
+        converted_timestamp = rreplace(timestamp, "-", ":-", 1)
+    converted_timestamp = rreplace(converted_timestamp, ":", "", 1)
+    return converted_timestamp
 
 
 # Extract datetime from string
 # Time - array of datetime strings
-def ExtractDatetimeData(Time):
-    return [datetime.datetime.strptime(ConvertTimestamp(x),
-                                       '%Y-%m-%dT%H:%M:%S:%z') for x in Time]
+def extract_datetime_data(time):
+    return [datetime.datetime.strptime(convert_timestamp(x),
+                                       '%Y-%m-%dT%H:%M:%S:%z') for x in time]
 
 
 # Function gets requested stock exchange data from barchart.com using free
 # getHistory API in .json format
-def GetMarketData(Period):
-    IntcData = GetIntelData(Period)
-    UsdPlnData = GetCurrencyData(Period)
-    if (Response_Status_Codes['OK'] == IntcData.status_code) and \
-       (Response_Status_Codes['OK'] == UsdPlnData.status_code):
+def get_market_data(period):
+    intc_data = get_intel_data(period)
+    usdpln_data = get_currency_data(period)
+    if (RESPONSE_STATUS_CODES['OK'] == intc_data.status_code) and \
+       (RESPONSE_STATUS_CODES['OK'] == usdpln_data.status_code):
         # TODO: try..catch exception if json fails
-        IntcData = IntcData.json()
-        UsdPlnData = UsdPlnData.json()
+        intc_data = intc_data.json()
+        usdpln_data = usdpln_data.json()
 
         # Extract only necessary data from response
-        IntcData = ExtractData(['timestamp', 'close'], IntcData['results'])
-        UsdPlnData = ExtractData(['timestamp', 'close'], UsdPlnData['results'])
+        intc_data = extract_data(['timestamp', 'close'], intc_data['results'])
+        usdpln_data = extract_data(['timestamp', 'close'],
+                                   usdpln_data['results'])
 
         # Format timestamp info from string to datatime.datetime object with
         # timezone awareness
-        IntcData[0] = ExtractDatetimeData(IntcData[0])
-        UsdPlnData[0] = ExtractDatetimeData(UsdPlnData[0])
+        intc_data[0] = extract_datetime_data(intc_data[0])
+        usdpln_data[0] = extract_datetime_data(usdpln_data[0])
 
-        return IntcData, UsdPlnData
+        return intc_data, usdpln_data
     else:
         return None
 
 
-def FindBestFittingElement(np_index, start_data_index, data, np_dt):
+def find_best_fitting_element(np_index, start_data_index, data, np_dt):
     end_data_index = len(data[0])
     return_done = False
 
@@ -243,39 +244,39 @@ def FindBestFittingElement(np_index, start_data_index, data, np_dt):
 
 
 # Function prepares data for matplotlib pyplot
-def PreparePlotData(period, Intc, UsdPln):
+def prepare_plot_data(period, intc, usdpln):
 
     # Determinate Start and End datetime:
-    EndDatetime = datetime.datetime.today()
+    end_datetime = datetime.datetime.today()
     # Add timezone awareness UTC+1
-    EndDatetime = EndDatetime.replace(tzinfo=datetime.timezone
-                                      (datetime.timedelta(hours=1)))
+    end_datetime = end_datetime.replace(tzinfo=datetime.timezone
+                                        (datetime.timedelta(hours=1)))
 
-    StartDatetime = EndDatetime - TimeDiffs[period]
+    start_datetime = end_datetime - TIME_DIFFS[period]
 
     # Prepare datetime axis
     # numpy datetime array data init
-    NpDatetime = numpy.arange(StartDatetime,
-                              EndDatetime,
-                              DeltaDatetimes[period],
-                              dtype=datetime.datetime)
+    np_datetime = numpy.arange(start_datetime,
+                               end_datetime,
+                               DELTA_DATETIMES[period],
+                               dtype=datetime.datetime)
 
     print("Datatime:")
-    print(StartDatetime)
-    print(EndDatetime)
+    print(start_datetime)
+    print(end_datetime)
 
     # lets do the magic...
 
     # create and init NpIntc, NpUsdPln, NpIntc2Pln numpy arrays
-    NpIntc = numpy.zeros(len(NpDatetime))
+    np_intc = numpy.zeros(len(np_datetime))
     # fill Intc numpy array with last value from INTC response
-    NpIntc.fill(Intc[1][-1])
+    np_intc.fill(intc[1][-1])
 
-    NpUsdPln = numpy.zeros(len(NpDatetime))
+    np_usdpln = numpy.zeros(len(np_datetime))
     # fill UsdPln numpy array with last value from ^USDPLN response
-    NpUsdPln.fill(UsdPln[1][-1])
+    np_usdpln.fill(usdpln[1][-1])
 
-    NpIntc2Pln = numpy.zeros(len(NpDatetime))
+    np_intc2pln = numpy.zeros(len(np_datetime))
 
     # Intc[0][] - contains datetime
     # Intc[1][] - contains data
@@ -285,39 +286,39 @@ def PreparePlotData(period, Intc, UsdPln):
     usdpln_data_index = 0
     usdpln_data_done = False
 
-    for dt_index in range(len(NpDatetime)):
+    for dt_index in range(len(np_datetime)):
         # print("Iter dt_index: ", dt_index,
         #      " datetime: ", NpDatetime[dt_index])
 
         # Find the best fitting data for NpDatetime element
         if intc_data_done is False:
-            NpIntc[dt_index], intc_data_index, intc_data_done = \
-                FindBestFittingElement(dt_index, intc_data_index,
-                                       Intc, NpDatetime)
+            np_intc[dt_index], intc_data_index, intc_data_done = \
+                find_best_fitting_element(dt_index, intc_data_index,
+                                          intc, np_datetime)
         if usdpln_data_done is False:
-            NpUsdPln[dt_index], usdpln_data_index, usdpln_data_done = \
-                FindBestFittingElement(dt_index, usdpln_data_index,
-                                       UsdPln, NpDatetime)
+            np_usdpln[dt_index], usdpln_data_index, usdpln_data_done = \
+                find_best_fitting_element(dt_index, usdpln_data_index,
+                                          usdpln, np_datetime)
 
     # calculate INTC value in PLN
-    NpIntc2Pln = NpIntc * NpUsdPln
+    np_intc2pln = np_intc * np_usdpln
 
-    return NpDatetime, NpIntc, NpUsdPln, NpIntc2Pln
+    return np_datetime, np_intc, np_usdpln, np_intc2pln
 
 
-def UpdatePlot(Period):
+def update_plot(period):
     # Fetch data from stock exchange market using barchart Web APi
-    Intc, UsdPln = GetMarketData(Period)
+    intc, usdpln = get_market_data(period)
 
     # prepare plot array
-    NpDatetime, NpIntc, NpUsdPln, NpIntc2Pln = PreparePlotData(Period,
-                                                               Intc, UsdPln)
+    np_datetime, np_intc, np_usdpln, np_intc2pln = \
+        prepare_plot_data(period, intc, usdpln)
 
     ax[0].clear()
     ax[0].set_ylabel("INTC(USD)")
     ax[0].set_title("Kurs INTC w USD")
     ax[0].grid(True, linestyle='--')
-    ax[0].plot_date(NpDatetime, NpIntc, 'r', xdate=True)
+    ax[0].plot_date(np_datetime, np_intc, 'r', xdate=True)
 
     ax[1].clear()
     ax2.clear()
@@ -326,10 +327,10 @@ def UpdatePlot(Period):
     ax[1].set_ylabel("INTC(PLN)")
     ax[1].set_title("Kurs INTC w PLN")
     ax[1].grid(True, linestyle='--')
-    ax[1].plot_date(NpDatetime, NpIntc2Pln, 'b', xdate=True)
+    ax[1].plot_date(np_datetime, np_intc2pln, 'b', xdate=True)
 
     ax2.set_ylabel("^USDPLN")
-    ax2.plot_date(NpDatetime, NpUsdPln, 'g:', xdate=True, linewidth=0.7)
+    ax2.plot_date(np_datetime, np_usdpln, 'g:', xdate=True, linewidth=0.7)
 
     plt.draw()
     return
@@ -342,19 +343,19 @@ class ButtonClickProcessor(object):
         return
 
     def process(self, event):
-        UpdatePlot(self.button.label.get_text())
+        update_plot(self.button.label.get_text())
         return
 
 
-def CreateButtons(Periods):
+def create_buttons(periods):
 
-    width = 1/len(Periods)
+    width = 1/len(periods)
     bx = []
     buttons = []
     left = 0.0
-    for index in range(len(Periods)):
+    for index in range(len(periods)):
         bx.append(plt.axes([left, 0.0, width, 0.05]))
-        buttons.append(ButtonClickProcessor(bx[-1], Periods[index]))
+        buttons.append(ButtonClickProcessor(bx[-1], periods[index]))
         left += width
 
     return bx, buttons
@@ -369,9 +370,9 @@ if __name__ == "__main__":
     ax2 = ax[1].twinx()
 
     # provide data to plot
-    UpdatePlot(Periods[3])
+    update_plot(PERIODS[3])
 
     # create buttons
-    bx, buttons = CreateButtons(Periods)
+    bx, buttons = create_buttons(PERIODS)
 
     plt.show()
